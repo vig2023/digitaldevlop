d08cbe253ad709cf5c501b41359797ff755a8255
vig2023 <114741486+vig2023@users.noreply.github.com>
2022-09-30 19:23:07 +0530

    Update testPlugin.js

--- testPlugin.js
+++ testPlugin.js
@@ -1,51 +1,19 @@
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
-  function InitializeMap() {
-    directionsDisplay = new google.maps.DirectionsRenderer();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions =
-{
-    zoom: 8,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+import GMapsAPI from "/GoogleMapsPluginApi.js"
 
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('directionpanel'));
-
-    var control = document.getElementById('control');
-    control.style.display = 'block';
-}
 const plugin = ({dashboard, simulator}) => {
-    var directionsDisplay;
-    var directionsService = new google.maps.DirectionsService();
+    GMapsAPI(
+        "AIzaSyD_WxxqknIQR40Bp7_d5gnucZ0P-X8XwWk",
+        dashboard.create(1, 2), [
+        {
+            lat: 48.813254159291475,
+            lng: 9.212379215835714
+        },
+        {
+            lat: 49.20261646797924,
+            lng: 9.189121574828052
+        },
+    ])
 
-    InitializeMap();
-    
-    const container = document.createElement("div")
-    container.setAttribute("style", `display:flex; height: 100%; width: 100%;`)
-    container.innerHTML = (`
-    <div style="max-width: fit-content; margin: 0 auto; position: relative; margin: auto 0;">
-        <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FDashboardSpeed.jpg?alt=media&token=44b42de9-12a7-4cb1-b525-94e8fc077141" style="height: fit-content; width: 100%; object-fit: contain;">
-        <div class="status-text" style="position: absolute;color: white;font-family: 'Lato';width: 100%;top: 0;height: 100%;box-sizing: border-box;display: flex;justify-content: center;padding-top: 10px;"></div>
-    </div>
-    `)
-
-    dashboard.create(7, 8).injectNode(container)
-
-    let currentValue = ""
-
-    simulator("Vehicle.Cabin.InstrumentPanel.Status", "get", async ({args}) => {
-        return currentValue
-    })
-
-    simulator("Vehicle.Cabin.InstrumentPanel.Status", "set", async ({args}) => {
-        currentValue = args[0]
-        container.querySelector(".status-text").textContent = currentValue
-        return null
-    })
-
-    
     return {}
 }
 
