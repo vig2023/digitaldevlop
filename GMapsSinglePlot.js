515f7b06b6a6aabee8494696a870816fe34fc22d
APAC\BZD1KOR <Balasubramanian.SudalaiMuthuVignesh@in.bosch.com>
2022-10-04 16:43:13 +0530

    changed-func-name

--- GMapsSinglePlot.js
+++ GMapsSinglePlot.js
@@ -1,9 +1,9 @@
-import GoogleMapsSinglePlotApi from "/GoogleMapsSinglePlotApi.js"
+import GoogleMapsSinglePlot from "/GoogleMapsSinglePlotApi.js"
 
 const plugin = ({grid, simulator}) => {
     return {
-        createDirections: (path) => {
-            GoogleMapsSinglePlotApi("AIzaSyD_WxxqknIQR40Bp7_d5gnucZ0P-X8XwWk", grid.create(1, 2), path)
+        plotMarker: (path) => {
+            GoogleMapsSinglePlot("AIzaSyD_WxxqknIQR40Bp7_d5gnucZ0P-X8XwWk", grid.create(1, 2), path)
         }
     }
 }
