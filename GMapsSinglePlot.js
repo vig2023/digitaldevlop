f2eff96904dc928ee040b0f90f53c95a0788e0fd
APAC\BZD1KOR <Balasubramanian.SudalaiMuthuVignesh@in.bosch.com>
2022-10-05 16:25:36 +0530

    grid-position-change

--- GMapsSinglePlot.js
+++ GMapsSinglePlot.js
@@ -3,7 +3,7 @@ import GoogleMapsSinglePlot from "./GoogleMapsSinglePlotApi.js"
 const plugin = ({grid, simulator}) => {
     return {
         plotMarker: (path) => {
-            GoogleMapsSinglePlot("AIzaSyD_WxxqknIQR40Bp7_d5gnucZ0P-X8XwWk", grid.create(1, 2), path)
+            GoogleMapsSinglePlot("AIzaSyD_WxxqknIQR40Bp7_d5gnucZ0P-X8XwWk", grid.create(3, 5), path)
         }
     }
 }
