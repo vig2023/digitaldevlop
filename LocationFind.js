a416ec42549be6f0c761a3b66d46f9a27ebba9cf
APAC\BZD1KOR <Balasubramanian.SudalaiMuthuVignesh@in.bosch.com>
2022-10-06 01:24:22 +0530

    done-changes-for-locater

--- LocationFind.js
+++ LocationFind.js
@@ -3,7 +3,7 @@ const plugin = ({grid, simulator}) => {
     container.setAttribute("style", `display:flex; height: 100%; width: 100%;`)
     container.innerHTML = (`
     <div style="max-width: fit-content; margin: 0 auto; position: relative; margin: auto 0;">
-        <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FLocate%20My%20Car%20%E2%80%93%20asset.png?alt=media&token=348aca00-8357-4102-a0ad-4468697c4a3b" style="height: fit-content; width: 100%; object-fit: contain;">
+        <img style="width:18em" src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FLocate%20My%20Car%20%E2%80%93%20asset.png?alt=media&token=348aca00-8357-4102-a0ad-4468697c4a3b" style="height: fit-content; width: 100%; object-fit: contain;">
        
     </div>
     `)
