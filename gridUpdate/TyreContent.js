fa511f3bc41519b6f588cb85ed8a92f7b9c3be7f
APAC\BZD1KOR <Balasubramanian.SudalaiMuthuVignesh@in.bosch.com>
2022-10-10 17:55:49 +0530

    tyre-color-style-change-01

--- gridUpdate/TyreContent.js
+++ gridUpdate/TyreContent.js
@@ -5,12 +5,16 @@ const plugin = ({widgets, simulator}) => {
     <div style="max-width: fit-content; margin: 0 auto; position: relative; margin: auto 0;">
        
         <div class="status-text" style="position: absolute;color: black;font-family: 'Lato';width: 100%;top: 0;height: 100%;box-sizing: border-box;display: flex;justify-content: center;padding-top: 10px;">
-        <p style="color:"#00568f";font-weight:600;font-size:20px">Tyre Pressure</p>
+        <div>
+        <p style="color:#00568f;font-weight:600;font-size:20px;font-family: 'Lato';">Tyre Pressure</p>
+        </div>
         <div style="display:flex">
-        <p>Front Left: <span style="color:red">14 psi</span></p>
-        <p>Front Right: <span style="color:red">40 psi</span></p>
-        <p>Rear Left: <span style="color:red">40 psi</span></p>
-        <p>Rear Right: <span style="color:red">40 psi</span></p>
+        <p>Front Left: <span style="color:red;font-family: 'Lato'">14 psi</span></p>
+        <p>Front Right: <span style="color:#1973a5;font-family: 'Lato'">40 psi</span></p>
+        </div>
+        <div style="display:flex">
+        <p>Rear Left: <span style="color:#1973a5;font-family: 'Lato'">40 psi</span></p>
+        <p>Rear Right: <span style="color:#1973a5;font-family: 'Lato'">40 psi</span></p>
         </div>
         </div>
     </div>
