43c2d5ec8e29af203b5a12fb73424f367dc6849b
APAC\BZD1KOR <Balasubramanian.SudalaiMuthuVignesh@in.bosch.com>
2022-10-10 18:24:40 +0530

    added-pressureUpdate

--- gridUpdate/UpdatePressure.js
+++ gridUpdate/UpdatePressure.js
@@ -2,19 +2,19 @@ const plugin = ({widgets, simulator}) => {
     const container = document.createElement("div")
     container.setAttribute("style", `height: 100%; width: 100%;`)
     container.innerHTML = (`
-    <div style="max-width: fit-content; margin: 0 auto; position: relative; margin: auto 0;">
+    <div style="max-width: fit-content;position: relative; margin: auto;">
        
         <div class="status-text" style="position: absolute;color: black;font-family: 'Lato';width: 100%;top: 0;height: 100%;box-sizing: border-box;display: inline-grid;justify-content: center;padding-top: 10px;">
         <div >
         <p style="color:#00568f;font-weight:600;font-size:20px;font-family: 'Lato';">Tyre Pressure</p>
         </div>
         <div style="display:block">
-        <p>F_Left: <span style="color:red;font-family: 'Lato'">14 psi</span></p>
-        <p>F_Right: <span style="color:#1973a5;font-family: 'Lato'">40 psi</span></p>
+        <p>F_Left: <span style="color:red;font-family: 'Lato';font-size:12px;font-weight:800">14 psi</span></p>
+        <p>F_Right: <span style="color:#1973a5;font-family: 'Lato';font-size:12px;font-weight:800">40 psi</span></p>
         </div>
         <div style="display:block">
-        <p>R_Left: <span style="color:#1973a5;font-family: 'Lato'">40 psi</span></p>
-        <p>R_Right: <span style="color:#1973a5;font-family: 'Lato'">40 psi</span></p>
+        <p>R_Left: <span style="color:#1973a5;font-family: 'Lato';font-size:12px;font-weight:800">40 psi</span></p>
+        <p>R_Right: <span style="color:#1973a5;font-family: 'Lato';font-size:12px;font-weight:800">40 psi</span></p>
         </div>
         </div>
     </div>
@@ -22,7 +22,7 @@ const plugin = ({widgets, simulator}) => {
 
     let boxGlobal = null
 
-    widgets.register("tyreupdate", (box) => {
+    widgets.register("uppressure", (box) => {
         boxGlobal = box
         box.injectNode(container)
         return () => {
