0090eea761ba35dab98e0eee4145413e5de2f302
APAC\BZD1KOR <Balasubramanian.SudalaiMuthuVignesh@in.bosch.com>
2022-11-07 16:36:15 +0530

    initial-design-push21

--- gridUpdate/maps/carInput.js
+++ gridUpdate/maps/carInput.js
@@ -3,24 +3,39 @@ const plugin = ({widgets, simulator}) => {
     container.setAttribute("style", `height: 100%; width: 100%;`)
     container.innerHTML = (`
     <div style="max-width: fit-content; margin: 0 auto; position: relative;">
+    <style>
+    .button {
+        background-color: #4c83af;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+    </style>
     <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FLocate%20My%20Car%20%E2%80%93%20asset.png?alt=media&token=348aca00-8357-4102-a0ad-4468697c4a3b" style="height: 100%; width: 100%; object-fit: contain;">
     <div style="position: relative;max-width: fit-content;bottom: 31em;
     left: 9%;">
-    <iframe
-    width="81%"
-    height="68%"
-    frameborder="0"
-    scrolling="no"
-    marginheight="0"
-    marginwidth="0"
-    src="https://maps.google.com/maps?q=27.149699,8.3672&hl=en-US&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    <label for="lname">Car Location:</label><br>
+    <input type="text" id="lname" name="lname" value="Robert Bosch GmbH, Bismarckstraße 71, 10627 Berlin, Germany" style="
+    width: 50%;
+    line-height: 3em;
+    font-size: 18px;
+">
+    
+
+<button class="button">Default Button</button>
     </div>   
     </div>
     `)
 
     let boxGlobal = null
 
-    widgets.register("cariframe", (box) => {
+    widgets.register("carInput", (box) => {
         boxGlobal = box
         box.injectNode(container)
         return () => {
